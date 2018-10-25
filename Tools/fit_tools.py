fix error_2 normalization

Tools/fit_tools.py
@@ -92,9 +92,8 @@ def error_2( m_d, mDot_d, m_m, mDot_m ):
 
     # clip data to active region
     I_c = np.where( (m_d < 0.98*m_d[0])*(m_d > 1.02*m_d[-1]) )
-
-    e_2_m = np.sum( (m_d[I_c] - m_m[I_c])**2 )/len(I_c)
-    e_2_mDot = np.sum( (mDot_d[I_c] - mDot_m[I_c])**2 )/len(I_c)
+    e_2_m = np.sum( (m_d[I_c] - m_m[I_c])**2 )/np.sqrt(len(I_c[0]))
+    e_2_mDot = np.sum( (mDot_d[I_c] - mDot_m[I_c])**2 )/np.sqrt(len(I_c[0]))
 
     return e_2_m, e_2_mDot
 