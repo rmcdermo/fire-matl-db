a36119fae79ea77d8d690e1accea13a8010a6912
rmcdermo <randy.mcdermott@gmail.com>
2018-10-25 11:49:36 -0400

    Tools: add Morgan's TGA tools

---
+++ Tools/fit_tools.py
@@ -0,0 +1,184 @@
+"""
+
+Module containing functions for fitting TGA data
+
+"""
+
+import numpy as np
+from scipy.integrate import odeint
+from scipy.signal import savgol_filter
+
+from matplotlib import pyplot as plt
+
+# Constants
+R   = 8.314     # J/mol-K
+e   = np.exp(1)
+
+# Computes RHS for 'odeint' assuming series mechanism for 
+def series_mechanism(m, T, K, beta):
+
+    # parse parameters
+    N_r     = K.shape[0]
+    A       = K[0,:]
+    E       = K[1,:]
+    nu      = K[2,:]
+
+    # compute rate constants
+    r       = A*np.exp(-E/(R*T))/beta
+
+    # compute RHS
+    dm_dT   = np.zeros(N_r + 1)
+
+    dm_dT[0] = -r[0] * m[0]
+    
+    for i in range(1, N_r):
+        dm_dT[i] = -r[i]*m[i] + nu[i-1]*r[i-1]*m[i-1]
+    dm_dT[-1] = nu[-1]*r[-1]*m[-2]
+
+    return dm_dT
+
+# Computes RHS for 'odeint' assuming parallel mechanism for 
+def parallel_mechanism(m, T, K, beta):
+
+    # parse parameters
+    N_r     = K.shape[1]
+    A       = K[0,:]
+    E       = K[1,:]
+
+    # compute rate constants
+    r       = A*np.exp(-E/(R*T))/beta
+
+    # compute RHS
+    dm_dT       = np.zeros(N_r + 1)
+    dm_dT[0:-1] = -r*m[0:-1]
+    dm_dT[-1]   = -np.sum( r*m[0:-1] )
+    
+    return dm_dT
+
+# Model prediction
+def predict_tga( mech, K, beta, m_0, T ):
+    
+    if mech == 'series':
+        m_ar    = odeint( series_mechanism, m_0, T, args=(K, beta) )
+        m       = np.sum( m_ar, 1 ) 
+    
+    if mech == 'parallel':
+        m_ar    = odeint( parallel_mechanism, m_0, T, args=(K, beta) )
+        m       = m_ar[:,-1]
+
+    return m 
+
+# Compute smoothed mass loss rate
+def smooth_mlr( T, m, dT_c ):
+   
+    temp_range  = dT_c
+    pt_den      = len(T)/(T[-1] - T[0])
+    win_len     = int(np.ceil(temp_range*pt_den)//2*2 + 1)
+    mDot_sm     = -savgol_filter(m, window_length=win_len, polyorder=2, 
+                                 deriv=1, delta=T[1]-T[0])
+    
+    return mDot_sm
+
+# get kinetic parameters from shape parameters
+def shape2arrhenius( T_p, delT, beta ):
+    
+    E   = R*T_p*T_p/delT
+    A   = (beta/delT)*np.exp( T_p/delT )
+
+    return A, E
+
+# compute L_2 error
+def error_2( m_d, mDot_d, m_m, mDot_m ):
+
+    # clip data to active region
+    I_c = np.where( (m_d < 0.98*m_d[0])*(m_d > 1.02*m_d[-1]) )
+
+    e_2_m = np.sum( (m_d[I_c] - m_m[I_c])**2 )/len(I_c)
+    e_2_mDot = np.sum( (mDot_d[I_c] - mDot_m[I_c])**2 )/len(I_c)
+
+    return e_2_m, e_2_mDot
+
+def process_tga( T, m, dT_c, eps_c ):
+
+    # get data parameters
+    N       = len(T)            # number of data points
+    h       = T[1] - T[0]       # temperature step size
+    dI_c    = int(dT_c/h)       # number of data points corresponding to crtical temp. width
+    
+    # compute smoothed mass loss rate
+    mDot    = smooth_mlr( T, m, dT_c )
+    
+    # compute averaged initial and final masses over first and last dT_c
+    m_0     = np.mean( m[0:dI_c] )
+    m_f     = np.mean( m[-dI_c:] )
+    
+    # find peaks
+    i_max   = np.argmax(mDot)   # identify maximum mass loss rate
+    I_p     = [i_max]           # create array of peak indices
+    for i in range(dI_c, N-dI_c):
+        if ( all( mDot_j <= mDot[i] for mDot_j in mDot[i-dI_c:i+dI_c] )
+             and mDot[i] > eps_c*mDot[i_max] 
+             and i != i_max ):
+            I_p.append( i )
+   
+    # order and count peaks
+    I_p.sort()
+    N_r     = len(I_p)
+    
+    # get peak values
+    m_p     = m[I_p]
+    T_p     = T[I_p]
+    mDot_p  = mDot[I_p]
+    
+    # compute trough masses
+    m_t     = np.zeros( N_r + 1 )
+    m_t[0]  = m_0
+    m_t[-1] = m_f
+   
+    # solve for intermediate trough masses, A*m_t = b
+    if N_r > 1:
+        
+        # build trough mass matrices
+        d_low       = np.ones( N_r )/(e - 1.)
+        d_mid       = 2.*np.ones( N_r + 1 )
+        d_upp       = (e - 1.)*np.ones( N_r )
+        A           = np.diag( d_low, -1 ) + np.diag( d_mid, 0 ) + np.diag( d_upp, 1 )
+        A[0,0]      = 1.
+        A[0,1]      = 0.
+        A[-1,-1]    = 1.
+        A[-1,-2]    = 0.
+       
+        # build right-hand side
+        b           = np.zeros( N_r + 1 )
+        b[0]        = m_0
+        b[-1]       = m_f
+        b[1:-1]     = (e/(e - 1.))*m_p[0:-1] + e*m_p[1:]
+    
+        # force values for deep troughs
+        for i in range(0, N_r-1):
+            
+            i_dt    = np.argmin( mDot[I_p[i]:I_p[i+1]] ) + I_p[i]
+            
+            if mDot[i_dt] < eps_c*mDot[i_max]:
+                A[i+1,:]    = np.zeros( N_r + 1)
+                A[i+1,i+1]  = 1.
+                b[i+1]      = m[i_dt]
+    
+        # solve system for trough masses
+        m_t = np.linalg.solve( A, b )
+        
+    # compute reaction mass losses 
+    dm  = m_t[0:-1] - m_t[1:]
+    
+    return I_p, T_p, m_p, mDot_p, m_t, dm
+
+# point estimate of peak widths, Method 1
+def point_estimate( m_p, mDot_p, m_t ):
+
+    return (m_p - m_t[1:])/mDot_p
+
+# approximate point estimate of peak widths, Method 2
+def approx_point_estimate( mDot_p, m_t, dm ):
+
+    return dm/(e*mDot_p)
+
---
+++ Tools/tga_fit.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+"""
+
+Main script for fitting TGA data 
+
+"""
+
+import sys
+import numpy as np
+from matplotlib import pyplot as plt
+
+import fit_tools as ft
+
+# fit parameters
+dT_c    = 10.                   # critical temperature width, K
+eps_c   = 0.05                  # critical fraction of maximum mass loss rate
+
+# get input data
+file_in = str(sys.argv[1])
+file_label = file_in[:-4]
+dat_raw = np.loadtxt(file_in, skiprows=1, delimiter=',')
+beta    = float(sys.argv[2])    # heating rate, K/min
+
+print "==========================================================="
+print " "
+print " Fitting TGA data from file: ", file_in
+print "   at heating rate of ", beta, " K/min."
+print " "
+print "-----------------------------------------------------------"
+
+# parse input data
+T_d     = dat_raw[:,0] + 273.15     # data temperatures, K
+m_d     = dat_raw[:,1]/dat_raw[0,1] # normalized mass data
+beta    = beta/60.                  # heating rate, K/s
+
+# data parameters
+h_d     = T_d[1] - T_d[0]       # temperature step size, K
+N_d     = len( T_d )
+
+# compute smoothed mass loss rate
+mDot_d  = ft.smooth_mlr( T_d, m_d, dT_c )
+
+# process TGA data to find quantities of interest
+I_p, T_p, m_p, mDot_p, m_t, dm_m = ft.process_tga( T_d, m_d, dT_c, eps_c )
+
+# Method 1: point estimate
+dT_m1       = ft.point_estimate( m_p, mDot_p, m_t )
+A_m1, E_m1  = ft.shape2arrhenius( T_p, dT_m1, beta )
+K_m1        = np.vstack((A_m1, E_m1, dm_m))
+m_m1        = ft.predict_tga( 'parallel', K_m1, beta, np.append(dm_m, 1.), T_d )
+mDot_m1     = ft.smooth_mlr( T_d, m_m1, dT_c )
+
+# Method 2: point estimate
+dT_m2       = ft.approx_point_estimate( mDot_p, m_t, dm_m )
+A_m2, E_m2  = ft.shape2arrhenius( T_p, dT_m2, beta )
+K_m2        = np.vstack((A_m2, E_m2, dm_m))
+m_m2        = ft.predict_tga( 'parallel', K_m2, beta, np.append(dm_m, 1.), T_d )
+mDot_m2     = ft.smooth_mlr( T_d, m_m2, dT_c )
+
+# compute L_2 errors for masses
+e_m_m1, e_mDot_m1 = ft.error_2( m_d, mDot_d, m_m1, mDot_m1 )
+e_m_m2, e_mDot_m2 = ft.error_2( m_d, mDot_d, m_m2, mDot_m2 )
+
+# print results to terminal
+print " "
+print "Number of reactions = ", len(I_p)
+print " "
+print "Method 1 Results: "
+print "  L_2 Mass Error           = ", e_m_m1
+print "  L_2 Mass Loss Rate Error = ", e_mDot_m1
+print "  A (1/s)                  = ", K_m1[0,:]
+print "  E (kJ/mol)               = ", K_m1[1,:]
+print "  dm (-)                   = ", K_m1[2,:]
+print " "
+print "Method 2 Results: "
+print "  L_2 Mass Error           = ", e_m_m2
+print "  L_2 Mass Loss Rate Error = ", e_mDot_m2
+print "  A (1/s)                  = ", K_m2[0,:]
+print "  E (kJ/mol)               = ", K_m2[1,:]
+print "  dm (-)                   = ", K_m2[2,:]
+print " "
+print "==========================================================="
+
+# convert units for plotting
+mDot_d  = mDot_d*beta
+mDot_m1 = mDot_m1*beta
+mDot_m2 = mDot_m2*beta
+T_d     = T_d - 273.15
+
+# plotting parameters plot
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.rc('lines', linewidth=1.0)
+plt.rc('xtick', labelsize=18)
+plt.rc('ytick', labelsize=18)
+
+plt.figure(1)
+plt.plot(T_d, mDot_d, 'k-', label='Expt.')
+plt.plot(T_d, mDot_m1, 'r--', label='Method 1')
+plt.plot(T_d, mDot_m2, 'g--', label='Method 2')
+plt.xlabel(r"Temperature ($^{\circ}$ C)", fontsize=22)
+plt.ylabel(r"Residual Mass Loss Rate (1/s)", fontsize=22)
+plt.legend(loc=1, numpoints=1, prop={'size':16})
+plt.tight_layout()
+plt.savefig(file_label + "_mlr_fit.pdf")
+ 
+plt.figure(2)
+plt.plot(T_d, m_d, 'k-', label='Expt.')
+plt.plot(T_d, m_m1, 'r--', label='Method 1')
+plt.plot(T_d, m_m2, 'g--', label='Method 2')
+plt.xlabel(r"Temperature ($^{\circ}$ C)", fontsize=22)
+plt.ylabel(r"Residual Mass Fraction", fontsize=22)
+plt.legend(loc=1, numpoints=1, prop={'size':16})
+plt.tight_layout()
+plt.savefig(file_label + "_m_fit.pdf")
+
